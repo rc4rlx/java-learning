b5e33b67ae8f0478072ef241ce8ae7a503df25fb
Carl Jhon Villanueva <carlvillanueva.work@gmail.com>
2025-11-23 16:42:36 +0800

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+package helloworld;
+
+public class Main {
+    public static void main(String[] args){
+    System.out.println("Hello World");
+    System.out.println("Hello Github!");
+    System.out.println("11/23/2025 - 4:30PM PHT");
+
+}
+
+}
